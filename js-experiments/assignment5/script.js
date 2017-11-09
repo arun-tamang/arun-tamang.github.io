6ae550d583b593f69926b2fd58fca47b387ea13f
Arun Tamang <aruntamang3791@gmail.com>
2017-11-09 11:15:37 +0545

    js update

---
+++ js-experiments/assignment5/script.js
@@ -0,0 +1,144 @@
+var data = [
+   {
+       tagName: 'div',
+       className: 'test-class',
+       styles: {
+           width: "100px",
+           height: "100px",
+           backgroundColor: 'red'
+       },
+       children: [
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'blue'
+               },
+           },
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'brown',
+                   float: 'right'
+               },
+           }
+       ]
+   },
+   {
+       tagName: 'div',
+       className: 'test-class',
+       styles: {
+           width: "100px",
+           height: "100px",
+           backgroundColor: 'red'
+       },
+       children: [
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'blue'
+               },
+           },
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'brown',
+                   float: 'right'
+               },
+           },
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'pink',
+                   float: 'right'
+               },
+           }
+       ]
+   },
+   {
+       tagName: 'div',
+       className: 'test-class',
+       styles: {
+           width: "100px",
+           height: "100px",
+           backgroundColor: 'red'
+       },
+       children: [
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'blue'
+               },
+           },
+           {
+               tagName: 'div',
+               className: 'box',
+               styles: {
+                   width: "50px",
+                   height: "50px",
+                   backgroundColor: 'brown',
+                   float: 'right'
+               },
+           }
+       ]
+   }
+];
+
+var mainWrapper = document.getElementById('main-wrapper');
+function createNode(dataItem){
+
+}
+
+function node(parent){
+    var that = this;
+    this.createNode = function(dataItem){
+        var keys = Object.keys(dataItem);
+        for(var j = 0; j<keys.length; j++){
+            if(keys[j].toUpperCase()=='tagName'.toUpperCase()) {
+                this. container = document.createElement(dataItem[keys[j]]);
+                parent.appendChild(this.container);
+            }
+            if(keys[j].toUpperCase()=='className'.toUpperCase()){
+                this.container.setAttribute('class', dataItem[keys[j]]);
+            }
+            if(keys[j].toUpperCase()=='styles'.toUpperCase()) {
+                var styleKeys = Object.keys(dataItem[keys[j]]);
+                for (var k=0; k <styleKeys.length; k++){
+                    this.container.style[styleKeys[k]]  = dataItem[keys[j]][styleKeys[k]];
+                }
+            }
+            if(keys[j].toUpperCase()=='children'.toUpperCase()){
+                var childKeys = Object.keys(dataItem[keys[j]]);
+                for(var l = 0; l < childKeys.length; l++){
+                    //create child
+                    var childNode = new node(that.container);
+                    childNode.createNode(dataItem[keys[j]][childKeys[l]]);
+                }
+            }
+        }
+    }
+}
+
+
+var dataKeys = Object.keys(data);
+for(var i = 0; i<data.length; i++){
+    var newNode = new node(mainWrapper);
+    newNode.createNode(data[i]);
+}
